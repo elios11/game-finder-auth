f31cc1db1116bdebec3cbbee124d403afc69543b
Elías Ferreira <eliosferre3@hotmail.com>
2023-12-12 23:57:34 -0300

    Add keep alive ping again

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ const jsonServerAuth = require("json-server-auth");
 const router = jsonServer.router("db.json");
 const app = jsonServer.create();
 
+const PORT = process.env.PORT || 3000;
+
 app.db = router.db;
+
+const keepAlivePing = () => {
+    http.get(`http://localhost:${PORT}/keepalive`, (res) => {
+        console.log("Sent Keep Alive ping");
+        res.on("end", () => setTimeout(keepAlivePing, 1800000)); // Ping every 30 minutes
+    });
+};
+
+keepAlivePing();
 
 app.use(jsonServerAuth);
 app.use(router);
